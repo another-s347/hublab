747e20a9ad9312799ad2b138a86ad3cba1ec2c01
skye347 <s347419313@gmail.com>
2018-09-17 22:08:20 +0800

    filehub: add set action

--- src/main/scala/Data/package.scala
+++ src/main/scala/Data/package.scala
@@ -1,11 +1,38 @@
 import User.Session
 import io.vertx.lang.scala.json.{Json, JsonObject}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.Success
+import scala.util.{Failure, Success, Try}
 
 package object Data {
     case class File(fileName:String, prefix:String, data:JsonObject, user:String)
+    object File{
+        def fromJson(j:JsonObject,user:String):File={
+            val filename = Try(j.getString("filename")) match {
+                case Success(null) =>
+                    throw Exception.MessageConvertException("filename do not exist")
+                case Success(x) => x
+                case Failure(_: java.lang.ClassCastException) =>
+                    throw Exception.MessageConvertException("filename should be a string")
+            }
+            val prefix = Try(j.getString("prefix")) match {
+                case Success(null) =>
+                    throw Exception.MessageConvertException("prefix do not exist")
+                case Success(x) => x
+                case Failure(_: java.lang.ClassCastException) =>
+                    throw Exception.MessageConvertException("prefix should be a string")
+            }
+            val data = Try(j.getJsonObject("data")) match {
+                case Success(null) =>
+                    throw Exception.MessageConvertException("data do not exist")
+                case Success(x) => x
+                case Failure(_: java.lang.ClassCastException) =>
+                    throw Exception.MessageConvertException("data should be a obj")
+            }
+            File(filename,prefix,data,user)
+        }
+    }
 
     class FileHub extends Uri.Hub.HubTrait{
         override def apply(hubName:String,action: String, target: String, query: Map[String, Vector[String]], body: Option[JsonObject], identity: Session.Identity): Future[JsonObject] = {
@@ -15,7 +42,17 @@ package object Data {
             })
             action match {
                 case "get"=>get(queryInDB,target,identity.User.baseUserInfo.name)
+                case "set"=>set(target,body,identity.User.baseUserInfo.name)
                 case unknown=>Future.failed(new Exception(s"unknwon action:$unknown"))
+            }
+        }
+
+        private def set(target:String,body:Option[JsonObject],user:String):Future[JsonObject]={
+            if(body.isEmpty)
+                Future.failed(new Exception("body is empty"))
+            Global.DB.Data.SetFile(File.fromJson(body.get,user)) transform {
+                case Success(value)=>
+                    Success(Json.emptyObj().put("result","success"))
             }
         }
 
