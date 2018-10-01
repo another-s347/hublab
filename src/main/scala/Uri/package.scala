62335656b61f8b6601f4cf9e8a8d5ce06c34dc37
skye347 <s347419313@gmail.com>
2018-10-01 16:26:00 +0800

    update uri module

--- src/main/scala/Uri/package.scala
+++ src/main/scala/Uri/package.scala
@@ -1,17 +1,37 @@
 import User.Session.Identity
-import io.lemonlabs.uri._
+//import io.lemonlabs.uri._
 import io.vertx.lang.scala.json.JsonObject
-
+import io.vertx.scala.core.MultiMap
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.collection.mutable
 import scala.concurrent.Future
 
 package object Uri {
-    def apply(uriString:String,body:Option[JsonObject],identity:Identity):Future[JsonObject]={
-        val uri=Uri.parse(uriString)
-        uri match {
-            case UrlWithoutAuthority("hublab",PathParts(hub,action,target),query,_)=>
-                Hub(hub)(hub,action,target,query.paramMap,body,identity)
-            case _=>
-                Future.failed(new Exception("uri cannot be parsed using UrlWithoutAuthority"))
+    //    def apply(uriString: String, body: Option[JsonObject], identity: Identity, sessionId: String): Future[JsonObject] = {
+    //        val uri=Uri.parse(uriString)
+    //        uri match {
+    //            case UrlWithoutAuthority("hublab",PathParts(hub,action,target),query,_)=>
+    //                Hub(hub)(hub, action, target, query.paramMap, body, identity, sessionId)
+    //            case _=>
+    //                Future.failed(new Exception("uri cannot be parsed using UrlWithoutAuthority"))
+    //        }
+    //    }
+
+    def apply(hubName: String, action: String, target: String, query: MultiMap, body: Option[JsonObject], sessionId: String): Future[JsonObject] = {
+        User.Session.GetOnlineIdentity(sessionId) flatMap { identity =>
+            Hub(hubName)(hubName, action, target, ConvertMultiMap(query), body, identity, sessionId)
         }
+    }
+
+    def apply(hubName: String, action: String, target: String, query: Map[String, Vector[String]], body: Option[JsonObject], sessionId: String): Future[JsonObject] = {
+        User.Session.GetOnlineIdentity(sessionId) flatMap { identity =>
+            Hub(hubName)(hubName, action, target, query, body, identity, sessionId)
+        }
+    }
+
+    private def ConvertMultiMap(query: MultiMap): Map[String, Vector[String]] = {
+        val mutableMap = new mutable.HashMap[String, Vector[String]]()
+        query.names().foreach(key => mutableMap += (key -> query.getAll(key).toVector))
+        mutableMap.toMap
     }
 }
